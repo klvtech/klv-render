332328f0c0e2675f35515d1e766f5b870eceab19
klvtech <klvtech@users.noreply.github.com>
2026-08-17 18:13:51 -0300

    Adiciona CORS para permitir chamadas do app no navegador

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import cors from 'cors'
 import { renderProjeto } from './render.js'
 
 const app = express()
+app.use(cors())
 app.use(express.json({ limit: '80mb' }))
 
 app.get('/health', (req, res) => {
